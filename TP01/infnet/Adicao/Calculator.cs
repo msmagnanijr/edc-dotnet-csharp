423dcaf7b8d9695446b1bc13769fb17dc2308ed0
Mauricio Magnani <msmagnanijr@gmail.com>
2022-02-01 22:21:26 -0300

    first commmit
    Signed-off-by: Mauricio Magnani <msmagnanijr@gmail.com>

---
+++ TP01/infnet/Adicao/Calculator.cs
@@ -0,0 +1,39 @@
+﻿//TP1 - Calcula a adição de dois números
+using System;
+namespace infnet
+{
+    class Calculator
+    {
+        static void Main()
+        {
+            try
+            {
+
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+                Console.WriteLine("Ambiente configurado para os padrões Brasileiros! Utilize ',' para números com casas decimais ou o resultado será inesperado!\n");
+
+                Console.WriteLine("Essa aplicação calcula a adição de dois números!\n");
+
+                Console.WriteLine("Digite o primeiro número:");
+                var firstValue = Console.ReadLine();
+
+                Console.WriteLine("Digite o segundo número:");
+                var secondValue = Console.ReadLine();
+                //TODO: Validar se o usuário utilizou corretamente o separador de casa decimais
+                if (double.TryParse(firstValue, out double firttNumber) && double.TryParse(secondValue, out double secondNumber))
+                {
+                    var result = firttNumber + secondNumber;
+                    Console.WriteLine($"O resultado da adição dos números {firttNumber} e {secondNumber} é: {result:0.##} !");
+                }
+                else {
+                    throw new ArgumentException("O valor digitado é inválido!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ TP01/infnet/Multiplicacao/Calculator.cs
@@ -0,0 +1,40 @@
+﻿//TP1 - Calcula a multiplicação de dois números
+using System;
+namespace infnet
+{
+    class Calculator
+    {
+        static void Main()
+        {
+            try
+            {
+
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+                Console.WriteLine("Ambiente configurado para os padrões Brasileiros! Utilize o caractere ',' para números com casas decimais ou o resultado será inesperado!\n");
+
+                Console.WriteLine("Essa aplicação calcula a multiplicação de dois números!\n");
+
+                Console.WriteLine("Digite o primeiro número:");
+                var firstValue = Console.ReadLine();
+
+                Console.WriteLine("Digite o segundo número:");
+                var secondValue = Console.ReadLine();
+                //TODO: Validar se o usuário utilizou corretamente o separador de casa decimais
+                if (double.TryParse(firstValue, out double firttNumber) && double.TryParse(secondValue, out double secondNumber))
+                {
+                    var result = firttNumber * secondNumber;
+                    Console.WriteLine($"O resultado da multiplicação dos números {firttNumber} e {secondNumber} é: {result:0.##} !");
+                }
+                else
+                {
+                    throw new ArgumentException("O valor digitado é inválido!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
