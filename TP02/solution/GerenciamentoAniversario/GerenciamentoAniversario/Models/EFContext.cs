5c44a14cdfb3fbdcee9298742f0468c4b585a2fd
Mauricio Magnani <msmagnanijr@gmail.com>
2022-02-04 21:49:24 -0300

    add TP02
    Signed-off-by: Mauricio Magnani <msmagnanijr@gmail.com>

---
+++ TP02/solution/GerenciamentoAniversario/GerenciamentoAniversario/Models/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoAniversario.Models
+{
+    public class EFContext : DbContext
+    {
+        private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=infnet;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+    }
+}
---
+++ TP02/solution/GerenciamentoAniversario/GerenciamentoAniversario/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoAniversario
+{
+    public static class Utilities
+    {
+
+        public static int diasAniversario(DateTime dataNascimento)
+        {
+            var data = dataNascimento;
+            var hoje = DateTime.Now;
+            var futuro = Convert.ToDateTime($"{data.Day}, {data.Month}, {hoje.Year}");
+            DateTime dataFuturo = Convert.ToDateTime($"{data.Day}, {data.Month}, {hoje.Year + 1}");
+            
+            int resultado = (futuro - hoje).Days;
+
+            if (resultado < 0)
+            {
+                futuro = Convert.ToDateTime($"{data.Day}, {data.Month}, {dataFuturo.Year}");
+            }
+
+            resultado = (futuro - hoje).Days;
+            
+            return resultado;
+        }
+    }
+}
